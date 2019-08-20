35b05ce2f8d06482fc17db824cbcf6d2c811fc67
root <root@DESKTOP-EQ2OFGT.localdomain>
2019-08-20 09:43:32 -0600

    forms

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert("Loading courses failed" + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }, []);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    course: newCourse,
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapDispatchtoProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchtoProps
+)(ManageCoursePage);
